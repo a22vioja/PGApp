b900a82ab6096fbabfe6c7c437efc0be7a79413f
a22vioja <a22vioja@student.his.se>
2023-05-08 22:22:35 +0200

    added MainActivity button functionality to go to AboutPage

--- app/src/main/java/com/example/pgapp/Bonsai.java
+++ app/src/main/java/com/example/pgapp/Bonsai.java
@@ -1,32 +1,33 @@
 package com.example.pgapp;
 
 public class Bonsai {
-
-    public int height;
+    private int height;
+    private int age;
     private String ID;
     private String name;
 
-
-
-    public String getName() {
-        return name;
+    public int getHeight() {
+        return height;
     }
-    public void setName(String name) {
-        this.name = name;
+    public void setHeight(int height) {
+        this.height = height;
     }
-
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
     public String getID() {
         return ID;
     }
     public void setID(String ID) {
         this.ID = ID;
     }
-
-    public int getHeight() {
-        return height;
+    public String getName() {
+        return name;
     }
-
-    public void setHeight(int height) {
-        this.height = height;
+    public void setName(String name) {
+        this.name = name;
     }
 }
