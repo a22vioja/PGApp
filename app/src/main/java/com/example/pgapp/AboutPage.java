9edd0433ff1efe7aee2400ad1b0ba9925b411e76
a22vioja <a22vioja@student.his.se>
2023-05-08 22:26:11 +0200

    added button functionality from AboutPage to RecycleView

--- app/src/main/java/com/example/pgapp/AboutPage.java
+++ app/src/main/java/com/example/pgapp/AboutPage.java
@@ -1,7 +1,10 @@
 package com.example.pgapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AboutPage extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class AboutPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+
+        Button button= findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                Intent intent = new Intent(AboutPage.this, RecycleView.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/pgapp/RecycleView.java
+++ app/src/main/java/com/example/pgapp/RecycleView.java
@@ -1,7 +1,10 @@
 package com.example.pgapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RecycleView extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class RecycleView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycle_view);
+
     }
 }
